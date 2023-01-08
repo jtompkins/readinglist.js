7c8d124ba79e6a622ca1a8534f80028f3db5e855
Josh Tompkins <josh@joshtompkins.com>
2023-01-08 13:17:17 -0500

    Remove old pug mocking files

--- __mocks__/pug.js
+++
@@ -1,8 +0,0 @@
-const mockTemplateRenderer = jest.fn()
-const mockCompileFile = jest.fn(templatePath => mockTemplateRenderer)
-
-module.exports = {
-  mockTemplateRenderer,
-  mockCompileFile,
-  compileFile: mockCompileFile,
-}
